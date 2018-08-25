db28a573971b9b6d48c155d3b9fd0828cf581e88
Vitor <vitorpinto81@gmail.com>
2018-08-25 18:26:07 +0100

    small fix

--- src/ui/private/wire2d.hh
+++ src/ui/private/wire2d.hh
@@ -5,60 +5,60 @@
 #ifndef CIRCUITSIM_WIRE2D_HH
 #define CIRCUITSIM_WIRE2D_HH
 
-
+#include "../point2d.hpp"
+#include "../../private/basic_component.hh"
 #include "drawable.hh"
 #include "draw_context_view.hh"
 
 namespace circuitsim {
-    namespace ui {
+	namespace ui {
 
-        class wire2d final
-                : public drawable<wire2d, basic_component < wire2d, 2>>
+		class wire2d final
+			: public drawable<wire2d, basic_component < wire2d, 2>> {
+		public:
+			using base = drawable <wire2d, basic_component<wire2d, 2>>;
+			using base::base;
 
-    {
-        public:
-        using base = drawable <wire2d, basic_component<wire2d, 2>>;
-        using base::base;
+			const point2d &end1() const {
+				return end1_;
+			}
 
-        const point2d &end1() const {
-            return end1_;
-        }
+			const point2d &end2() const {
+				return end2_;
+			}
 
-        const point2d &end2() const {
-            return end2_;
-        }
+			friend point2d position(const wire2d &x, unsigned ix) {
+				return ix == 0 ? x.end1_ : x.end2_;
+			}
 
-        friend point2d position(const wire2d &x, unsigned ix) {
-            return ix == 0 ? x.end1_ : x.end2_;
-        }
+			friend void position(wire2d &x, unsigned ix, point2d val) {
+				if (ix == 0) {
+					x.end1_ = val;
+				}
+				else {
+					x.end2_ = val;
+				}
+			}
 
-        friend void position(wire2d &x, unsigned ix, point2d val) {
-            if (ix == 0) {
-                x.end1_ = val;
-            } else {
-                x.end2_ = val;
-            }
-        }
+		private:
+			point2d end1_;
+			point2d end2_;
+		};
+	}
 
-        private:
-        point2d end1_;
-        point2d end2_;
-    };
-}
+	template<>
+	struct component_traits<ui::wire2d> final {
 
-template<>
-struct component_traits<ui::wire2d> final {
+		static constexpr std::string_view symbol() {
+			return "*Wire*";
+		}
 
-    static constexpr std::string_view symbol() {
-        return "*Wire*";
-    }
-
-    static void draw(const ui::wire2d &src, ui::draw_context_view &ctx) {
-        auto[x1, y1] = src.end1();
-        auto[x2, y2] = src.end2();
-        ctx.line(x1, y1, x2, y2);
-    }
-};
+		static void draw(const ui::wire2d &src, ui::draw_context_view &ctx) {
+			auto[x1, y1] = src.end1();
+			auto[x2, y2] = src.end2();
+			ctx.line(x1, y1, x2, y2);
+		}
+	};
 }
 
 
